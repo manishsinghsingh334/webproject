d36523f0626f3e14e77e6830016fe61018d06afc
Manish singh <manishsingh.singh334@gmail.com>
2023-07-08 11:09:38 +0530

    materialui manish first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import {Button} from '@mui/material';
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>My name is manish</h1>
+      <div>
+      <Button variant="contained">Hello World</Button>
+    </div>
+    
+      
+    
+    </>
+  )
+}
+
+export default App
